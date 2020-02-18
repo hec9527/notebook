4c4c99dcf488196707cd021b8c7ac01ae824ba62
hec <chun.he@100credit.com>
2020-02-18 15:33:43 +0800

    新增前端路由history模式

---
+++ Web/前端路由/history/index.js
@@ -0,0 +1,44 @@
+/**
+ *     非常 hack 的方法
+ *
+ *     观察者模式  +  手动事件
+ *
+ */
+
+// // 观察者模式
+// function observe(obj, callBack = () => {}) {
+//     return new Proxy(obj, {
+//         set(target, key, value, receiver) {
+//             const result = Reflect.set(target, key, value, receiver);
+//             callBack(result, key, value);
+//             return result;
+//         }
+//     });
+// }
+
+// const obj = observe({ x: 1, y: 2 }, () => console.log('change it'));
+
+// // 路由模块
+// class Routers {
+//     constructor() {
+//         this.state = observe({}, this.emit);
+//         this.pushState = history.pushState;
+//         this.replaceState = history.replaceState;
+
+//         window.addEventListener('onhistorystatechange', () =>
+//             this.handlerStateChange()
+//         );
+//     }
+
+//     // 手动触发事件
+//     emit() {
+//         const event = new Event('onhistorystatechange');
+//         window.dispatchEvent(EventTarget);
+//     }
+
+//     handlerStateChange() {
+//         console.log('deal status change');
+//     }
+// }
+
+// const router = new Routers();
--- Web/前端路由/readme.md
+++ Web/前端路由/readme.md
@@ -31,3 +31,24 @@
   - URL看起来很凌乱
   - 导致页面锚点功能失效
   - 相同hash值不会触发   hashchange事件
+
+## hash模式
+
+- 类似于Tab，但是满足：修改URL不更新页面，触发事件
+
+## history模式
+
+### H5之前
+
+***H5之前的API只能用于多页面跳转，每次调用会导致页面刷新***
+
+- history.go(-1);       // 后退一页
+- history.go(2);        // 前进两页
+- history.forward();    // 前进一页
+- history.back();       // 后退一页
+
+### H5新增API
+
+- history.pushState();    // 添加新的状态到历史状态栈
+- history.replaceState(); // 用新的状态代替当前状态
+- history.state           // 返回当前状态对象
