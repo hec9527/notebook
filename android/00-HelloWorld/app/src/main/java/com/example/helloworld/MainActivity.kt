c4aee81571e2c542e4d26a5454ef22e8af718094
hec <hec9527@foxmail.com>
2021-04-08 13:56:14 +0800

    添加日志打印

--- android/00-HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ android/00-HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.v("MainActive", "onCreate executed")
     }
 }
