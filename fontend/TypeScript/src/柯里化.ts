705d2209c2a94667b50f65958f5ce72946e23bf5
hec <hec9527@foxmail.com>
2021-04-09 19:22:46 +0800

    更新文档: 柯里化

--- fontend/TypeScript/src/柯里化.ts
+++ fontend/TypeScript/src/柯里化.ts
@@ -1,7 +1,26 @@
 export default {};
 
-// function Currie(x: number, ...arr: number[]): number {
-//   if (arr) {
-//     return x + Currie.apply({}, arr);
-//   }
-// }
+function Curry(x: number) {
+  let res = x;
+  const add = (y: number) => {
+    res += y;
+    return add;
+  };
+
+  add.valueOf = () => res;
+
+  add.toString = () => `${res}`;
+
+  return add;
+}
+
+console.log(+Curry(1)(2)(3)(4)(5)(6)(7)(8)(9)(10));
+
+function currie(a: number) {
+  return function (b: number) {
+    return function (c: number) {
+      return a + b + c;
+    };
+  };
+}
+console.log(currie(1)(2)(3)); // 6
