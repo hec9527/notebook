7c7f3cbf651c28f79f205b951a3c679594a78ef4
hec9527 <hec9527@foxmail.com>
2021-05-25 20:03:14 +0800

    修改应用启动图标

--- android/15-2048/app/src/main/java/com/example/a2048/MainActivity.kt
+++ android/15-2048/app/src/main/java/com/example/a2048/MainActivity.kt
@@ -4,16 +4,15 @@ import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.webkit.WebChromeClient
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 
 const val URL = "https://hec9527.github.io/2048/src/"
 //const val URL = "http://192.168.1.16:5500/src/index.html"
 //const val URL = "http://192.168.1.16:5500/"
+// const val URL = "https://2048-hec9527.vercel.app/"
+// const val URL = "https://2048-eight.vercel.app/"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var webView: WebView
@@ -26,8 +25,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
-//        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//        window.statusBarColor = activity
+        //        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_IMMERSIVE or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+        //        window.statusBarColor = activity
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
 
         webView = findViewById(R.id.webview)
@@ -45,6 +44,17 @@ class MyWebClient : WebViewClient() {
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
         view?.loadUrl(request?.url.toString())
         return false
+    }
+
+    override fun onReceivedError(
+        view: WebView?, request: WebResourceRequest?, error: WebResourceError?
+    ) {
+        val url = "https://2048-hec9527.vercel.app"
+        if (request?.url.toString() == url) {
+            return super.onReceivedError(view, request, error)
+        } else {
+            view?.loadUrl(url)
+        }
     }
 }
 
