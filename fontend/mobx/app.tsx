a178fb2f5447137d994467a0b94b85209fd8119a
hec9527 <hec9527@foxmail.com>
2021-09-08 16:57:00 +0800

    删除无用的内容

---
+++ fontend/mobx/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      凑人数
+      {/*  */}
+    </div>
+  );
+};
+
+export default App;
---
+++ fontend/mobx/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+/** @type {import("webpack").Configuration} */
+const config = {
+  mode: 'development',
+
+  entry: './index.tsx',
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.js', '.json', 'ts', '.tsx'],
+  },
+
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new htmlWebpackPlugin({
+      title: 'mobx测试',
+      template: './index.html',
+    }),
+  ],
+};
+
+module.exports = config;
