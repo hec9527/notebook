215a5548f4d852f282a060b262affe289418caaf
hec9527 <hec9527@foxmail.com>
2024-05-15 10:38:23 +0800

    chore: 小改动

---
+++ dart/01-var.dart
@@ -0,0 +1,11 @@
+var name = 1;
+int count = 2;
+
+String s = count.toString();
+
+// print("$name",)
+
+void main() {
+  var name = 'sa';
+  return name;
+}
---
+++ dart/readme.md
@@ -0,0 +1,9 @@
+# Dart
+
+## 变量
+
+dart 允许使用var申明变量，或者指定具体类型
+```dart
+var count = 1;
+int number = 1;
+```
